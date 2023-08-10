63874515fc4281eeb859f4c7c95be8aa5a749003
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-08-10 12:36:26 -0300

    responsive footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,60 +1,68 @@
+import "./styles.css";
+
 export function Footer() {
   return (
-    <footer className="maxw">
-      <img src="" alt="" />
+    <footer className="maxw footer">
+      <img
+        src="./assets/footer-logo.png"
+        alt="Little Lemon logo"
+        className="footer-logo"
+      />
 
-      <div>
-        <h3>Doormat Navigation</h3>
-        <ul>
-          <a href="/">
-            <li>Home</li>
-          </a>
-          <a href="#about">
-            <li>About</li>
-          </a>
-          <a href="#menu">
-            <li>menu</li>
-          </a>
-          <a href="#reservations">
-            <li>Reservations</li>
-          </a>
-          <a href="#order-online">
-            <li>Order Online</li>
-          </a>
-          <a href="/login">
-            <li>Login</li>
-          </a>
-        </ul>
-      </div>
+      <div className="links-container">
+        <div>
+          <h3>Doormat Navigation</h3>
+          <ul>
+            <a href="/">
+              <li>Home</li>
+            </a>
+            <a href="#about">
+              <li>About</li>
+            </a>
+            <a href="#menu">
+              <li>menu</li>
+            </a>
+            <a href="#reservations">
+              <li>Reservations</li>
+            </a>
+            <a href="#order-online">
+              <li>Order Online</li>
+            </a>
+            <a href="/login">
+              <li>Login</li>
+            </a>
+          </ul>
+        </div>
 
-      <div>
-        <h3>Contact</h3>
-        <ul>
-          <a href="#Address">
-            <li>Address</li>
-          </a>
-          <a href="#phone">
-            <li>phone number</li>
-          </a>
-          <a href="#email">
-            <li>email</li>
-          </a>
-        </ul>
-      </div>
+        <div>
+          <h3>Contact</h3>
+          <ul>
+            <a href="#Address">
+              <li>Address</li>
+            </a>
+            <a href="#phone">
+              <li>phone number</li>
+            </a>
+            <a href="#email">
+              <li>email</li>
+            </a>
+          </ul>
+        </div>
 
-      <div>
-        <h3>Social Media Links</h3>
-        <ul>
-          <a href="#Address">
-            <li>Address</li>
-          </a>
-          <a href="#phone">
-            <li>phone number</li>
-          </a>
-          <a href="#email">
-            <li>email</li>
-          </a>
-        </ul>
+        <div>
+          <h3>Social Media Links</h3>
+          <ul>
+            <a href="#Address">
+              <li>Address</li>
+            </a>
+            <a href="#phone">
+              <li>phone number</li>
+            </a>
+            <a href="#email">
+              <li>email</li>
+            </a>
+          </ul>
+        </div>
       </div>
     </footer>
   );
